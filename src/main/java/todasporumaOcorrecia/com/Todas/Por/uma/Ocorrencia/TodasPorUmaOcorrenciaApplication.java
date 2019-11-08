6f5e01b3d0c6345b0a9eeb9d090e85a147a4977d
Mateus98 <mateus_lima98@hotmail.com>
2019-11-08 20:41:03 -0300

    Exercicios 12,13,14,15

---
+++ src/main/java/todasporumaOcorrecia/com/Todas/Por/uma/Ocorrencia/TodasPorUmaOcorrenciaApplication.java
@@ -0,0 +1,13 @@
+package todasporumaOcorrecia.com.Todas.Por.uma.Ocorrencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodasPorUmaOcorrenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodasPorUmaOcorrenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/todasporumaOcorrecia/com/Todas/Por/uma/Ocorrencia/TodasPorUmaOcorrenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package todasporumaOcorrecia.com.Todas.Por.uma.Ocorrencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodasPorUmaOcorrenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
